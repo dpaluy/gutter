38bf3d04034d0a3c432bf79ad08bc1cb2d7b69c1
David Paluy <david@paluy.org>
2014-07-27 21:07:50 -0400

    fix precompile

--- lib/gutter/engine.rb
+++ lib/gutter/engine.rb
@@ -6,5 +6,9 @@ require 'jquery-datatables-rails'
 module Gutter
   class Engine < ::Rails::Engine
     isolate_namespace Gutter
+
+    initializer "gutter.assets.precompile" do |app|
+      app.config.assets.precompile += %w(gutter/application.css gutter/application.js)
+    end
   end
 end
